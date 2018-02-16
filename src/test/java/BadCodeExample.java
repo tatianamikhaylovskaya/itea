8066eabfe19ee6d2e870e403d5c2441b21c98ef8
tatianamikhaylovskaya <mihaylovskaya@gmail.com>
2018-02-16 20:27:43 +0200

    Updated .ideaproject

---
+++ src/test/java/BadCodeExample.java
@@ -0,0 +1,34 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
+
+import static java.lang.Thread.sleep;
+
+public class BadCodeExample {
+
+    public static void main(String args[]) throws InterruptedException {
+            System.out.println("hello world");
+            WebDriver driver = new FirefoxDriver();
+            driver.get("https://www.google.com/");
+            driver.findElement(By.id("lst-ib")).sendKeys("Selenium");
+            driver.findElement(By.name("btnK")).click();
+            sleep(5000);
+
+            List<WebElement> list = driver.findElements(By.xpath("//*[@id=\"rso\"]/div/div/div/div/div/h3/a"));
+            System.out.println("Number of links: "+ list.size());
+            List<WebElement> seleniumList = driver.findElements(By.xpath("//*[@id=\"rso\"]/div/div/div/div/div/h3/a[contains(text(),'elenium')]"));
+            System.out.println("Number of Selenium links: "+ seleniumList.size());
+            if(list.size() == seleniumList.size()){
+                    System.out.println("All search results correct ");
+            } else {
+                    System.out.println("doesn't contain word Selenium");
+            }
+            driver.close();
+
+}
+
+}
